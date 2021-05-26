44e7efa3a98e0ac45394046971162650fb63f521
Nitin Pawar <nitin.pawar150@gmail.com>
2021-05-26 23:41:54 +0530

    commit

--- app/src/main/java/com/example/beatme/LandingActivity.java
+++ app/src/main/java/com/example/beatme/LandingActivity.java
@@ -31,8 +31,6 @@ public class LandingActivity extends AppCompatActivity {
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_tournaments, R.id.nav_match)
                 .setDrawerLayout(drawer)
